5e794621b398ae020d5c22b2375e31621555ec1a
deekra <114116356+deekra@users.noreply.github.com>
2022-10-22 23:35:36 -0700

    Update main.js

--- Contact Me/main.js
+++ Contact Me/main.js
@@ -1,1 +1,6 @@
+var name = prompt("What is your name?"); 
+var subject = prompt("subject");                             
+var additional = prompt(" Your message to me");                                             
+var msg = 'Thank you!';                           // Message
 
+el.innerHTML = msg;
