ad265cc417332a6bce85e8cdad0bb88e7365d52f
deekra <114116356+deekra@users.noreply.github.com>
2022-11-04 21:23:27 -0700

    Update main.js

--- Contact Me/main.js
+++ Contact Me/main.js
@@ -1,10 +1,10 @@
-var name =Name ; 
-var subject = prompt("Subject"); 
-var email = prompt("Email");
-var additional = prompt("Comment");                                             
+var name = Name ; 
+var subject = Subject; 
+var email = Email
+var additional = Comment;                                             
 var msg = 'Thank you!';   // Message
 
-<form action="index.html" method="get"
+<form action="Contact Me/index.html" method="get"
 name="myContact">
 
 </form>
